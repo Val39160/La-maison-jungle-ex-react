d386832f0075fa91fefc22af4b6b2625475a2d19
valentin corger <valentin.corger@gmail.com>
2024-09-02 15:58:17 +0200

    done the banner

---
+++ la-maison-jungle/src/components/App.js
@@ -0,0 +1,11 @@
+import Banner from './Banner';
+import logo from '../logo.svg';
+import '../App.css';
+
+function App() {
+  return (
+    <Banner/>
+  );
+}
+
+export default App;
---
+++ la-maison-jungle/src/components/Banner.js
@@ -0,0 +1,8 @@
+function Banner() {
+
+  return (
+    <h1>La Maison Jungle</h1>
+
+  );
+}
+export default Banner;
